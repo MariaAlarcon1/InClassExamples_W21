fa722cc09d6b4403580eada21293f5f0008a65e7
Eric Torunski <etorunski@gmail.com>
2021-01-29 09:43:53 -0500

    finished tv example

--- app/src/main/java/com/cst2335/inclassexamples_w21/MainActivity.java
+++ app/src/main/java/com/cst2335/inclassexamples_w21/MainActivity.java
@@ -1,50 +1,19 @@
 package com.cst2335.inclassexamples_w21;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear); //shows the layout
+        setContentView(R.layout.activity_main);
 
-        TextView myText = findViewById(R.id.text);//brings the textView from XML to Java
-        myText.setText(R.string.hello_world);//nullPointer Exception
-
-        EditText myEdit = findViewById(R.id.edit);
-        RadioButton myCb = findViewById(R.id.myCb);
-
-        Button myBtn = findViewById(R.id.btn);
-        myBtn.setOnClickListener((vw) -> {
-            myText.setText("You clicked the button!");
-            myCb.setChecked(false);
-
-
-           // Toast.makeText(MainActivity.this, " ",Toast.LENGTH_LONG).show();
-            Snackbar.make(myText, "hello snack", Snackbar.LENGTH_LONG).show();
-        });
-        ImageButton myImgBtn = findViewById(R.id.imgView);
-
-        myImgBtn.setOnClickListener((view) -> myEdit.setText("You clicked the image"));
-
-        myCb.setOnCheckedChangeListener((btnView, onOrOff)-> {
-            myBtn.setText(onOrOff?"box is on" : "box is off");
-
-        });
-   }
+        Button viewButton = findViewById(R.id.button2);
+        viewButton.setOnClickListener( click ->
+                startActivity(new Intent(MainActivity.this, VideoActivity.class)));
+    }
 }
