c6f199c7497cef85db5aadf30a383e336201f879
Eric Torunski <etorunski@gmail.com>
2021-02-12 07:54:44 -0500

    Revert "week3 Shared Preferences example"
    This reverts commit d4031565

--- app/src/main/java/com/cst2335/inclassexamples_w21/FirstActivity.java
+++ app/src/main/java/com/cst2335/inclassexamples_w21/FirstActivity.java
@@ -1,24 +1,19 @@
 package com.cst2335.inclassexamples_w21;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class FirstActivity extends AppCompatActivity {
-    private ArrayList<String> elements = new ArrayList<>( Arrays.asList( "one", "Two"/*Empty*/ ) );
+    private ArrayList<String> elements = new ArrayList<>( Arrays.asList( "One", "Two" ) );
     private MyListAdapter myAdapter;
 
     @Override
@@ -29,43 +24,24 @@ public class FirstActivity extends AppCompatActivity {
         // Before this function, the screen is empty.
         setContentView(R.layout.activity_main);
 
-        ListView myList = findViewById(R.id.theListView);
-        myList.setAdapter( myAdapter = new MyListAdapter());
-
-        myList.setOnItemLongClickListener( (p, b, pos, id) -> {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("A title")
-
-                    //What is the message:
-                    .setMessage("Do you want to add stuff")
-
-                    //what the Yes button does:
-                    .setPositiveButton("Yes", (click, arg) -> {
-                        elements.add("HELLO");
-                        myAdapter.notifyDataSetChanged();
-                    })
-                    //What the No button does:
-                    .setNegativeButton("No", (click, arg) -> { })
-
-                    //An optional third button:
-                    .setNeutralButton("Maybe", (click, arg) -> {  })
-
-                    //You can add extra layout elements:
-                    .setView(getLayoutInflater().inflate(R.layout.row_layout, null) )
-
-                    //Show the dialog
-                    .create().show();
-            return true;
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener( click -> {
+            elements.add("Hi");
+            myAdapter.notifyDataSetChanged();
         });
 
-        //Whenever you swipe down on the list, do something:
-        SwipeRefreshLayout refresher = findViewById(R.id.refresher);
-        refresher.setOnRefreshListener( () -> refresher.setRefreshing(false)  );
+        ListView myList = findViewById(R.id.theListView);
+        myList.setAdapter( myAdapter = new MyListAdapter());
+        myList.setOnItemClickListener( (parent, view, pos, id) -> {
+
+            elements.remove(pos);
+            myAdapter.notifyDataSetChanged();
+        }   );
     }
 
-    private class MyListAdapter extends BaseAdapter{
+    private class MyListAdapter extends BaseAdapter {
 
-        public int getCount() { return elements.size(); }
+        public int getCount() { return elements.size();}
 
         public Object getItem(int position) { return "This is row " + position; }
 
@@ -73,14 +49,11 @@ public class FirstActivity extends AppCompatActivity {
 
         public View getView(int position, View old, ViewGroup parent)
         {
-            View newView = old;
             LayoutInflater inflater = getLayoutInflater();
 
             //make a new row:
-            if(newView == null) {
-                newView = inflater.inflate(R.layout.row_layout, parent, false);
+            View newView = inflater.inflate(R.layout.row_layout, parent, false);
 
-            }
             //set what the text should be for this row:
             TextView tView = newView.findViewById(R.id.textGoesHere);
             tView.setText( getItem(position).toString() );
