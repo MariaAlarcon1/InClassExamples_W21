3d481bbe15df034399e561d4cd1a50a62ce8c1f2
Eric Torunski <etorunski@gmail.com>
2021-02-25 21:32:59 -0500

    week6

--- app/src/main/java/com/cst2335/inclassexamples_w21/MainActivity.java
+++ app/src/main/java/com/cst2335/inclassexamples_w21/MainActivity.java
@@ -1,4 +1,5 @@
 package com.cst2335.inclassexamples_w21;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
@@ -22,12 +23,10 @@ public class MainActivity extends AppCompatActivity {
         MyHTTPRequest req = new MyHTTPRequest();
         req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
     }
-
-
     //Type1     Type2   Type3
     private class MyHTTPRequest extends AsyncTask< String, Integer, String>
     {
-        //Type3                Type1
+        //Type3                     Type1
         public String doInBackground(String ... args)
         {
             try {
@@ -40,6 +39,53 @@ public class MainActivity extends AppCompatActivity {
 
                 //wait for data:
                 InputStream response = urlConnection.getInputStream();
+
+
+
+                //From part 3: slide 19
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput( response  , "UTF-8");
+
+
+
+                //From part 3, slide 20
+                String parameter = null;
+
+                int eventType = xpp.getEventType(); //The parser is currently at START_DOCUMENT
+
+                while(eventType != XmlPullParser.END_DOCUMENT)
+                {
+
+                    if(eventType == XmlPullParser.START_TAG)
+                    {
+                        //If you get here, then you are pointing at a start tag
+                        if(xpp.getName().equals("Weather"))
+                        {
+                            //If you get here, then you are pointing to a <Weather> start tag
+                            String outlook = xpp.getAttributeValue(null,    "outlook");
+                            String windy = xpp.getAttributeValue(null, "windy");
+                        }
+
+                        else if(xpp.getName().equals("AMessage"))
+                        {
+                            parameter = xpp.getAttributeValue(null, "message"); // this will run for <AMessage message="parameter" >
+                        }
+                        else if(xpp.getName().equals("Weather"))
+                        {
+                            parameter = xpp.getAttributeValue(null, "outlook"); //this will run for <Weather outlook="parameter"
+                            parameter = xpp.getAttributeValue(null, "windy"); //this will run for <Weather windy="paramter"  >
+                        }
+                        else if(xpp.getName().equals("Temperature"))
+                        {
+                            xpp.next(); //move the pointer from the opening tag to the TEXT event
+                            parameter = xpp.getText(); // this will return  20
+                        }
+                    }
+                    eventType = xpp.next(); //move to the next xml event and store it in a variable
+                }
+
 
             }
             catch (Exception e)
